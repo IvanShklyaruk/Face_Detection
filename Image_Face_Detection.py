5c8e5d5b2051e3c2aaa8f562864f8b98060c4c90
Ivan <shklyaruk.ivan@gmail.com>
2021-03-31 21:13:41 -0400

    minor tweaks

--- Image_Face_Detection.py
+++ Image_Face_Detection.py
@@ -12,7 +12,7 @@ grayscaled_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 # Detect faces
 face_coordinates = trained_face_data.detectMultiScale(grayscaled_img)
 
-# Draw the rectangle around the faces
+# Draw the rectangles around the faces
 for (x, y, w, h) in face_coordinates:
     cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
 
