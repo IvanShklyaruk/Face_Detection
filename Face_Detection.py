88eaa8f153e15aa5e1843e56843874c77d30d6ab
Ivan <shklyaruk.ivan@gmail.com>
2021-03-31 21:06:12 -0400

    Final touches

--- Face_Detection.py
+++ Face_Detection.py
@@ -3,7 +3,7 @@ import cv2
 # Loaded data on frontal faces.
 trained_face_data = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
-# Chosen image to detect face in
+# Chosen image to detect faces in
 img = cv2.imread('RDJ.png')
 
 # Convert the image to greyscale
@@ -12,21 +12,10 @@ grayscaled_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 # Detect faces
 face_coordinates = trained_face_data.detectMultiScale(grayscaled_img)
 
-# Draw the rectangle around the face
-(x, y, w, h) = face_coordinates[0]
-cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
+# Draw the rectangle around the faces
+for (x, y, w, h) in face_coordinates:
+    cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
 
-
-
-
-
-
-
-
-
-
-
-cv2.imshow('Ivan Shklyaruk Face Detection', img)
+# Show the image with the faces
+cv2.imshow('Ivan Shklyaruk Image Face Detection', img)
 cv2.waitKey()
-
-print("Code Completed!")
